fdd69b23a5904931deb779d6ab7a92079a4ba06d
Scott Madeira <smadeira@geneva.edu>
2025-02-25 11:35:22 -0500

    Added major and minor endpoints

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\MajorController;
+use App\Http\Controllers\MinorController;
 
 Route::resource('courses', CourseController::class);
+Route::resource('majors', MajorController::class);
+Route::resource('majors', MinorController::class);
+
 
 
 Route::get('/', function () {
